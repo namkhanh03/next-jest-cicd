544f2a62b95b264ed7c09bbe1e38232d275640dc
namkhanh03 <tnkhanhs03@gmail.com>
2025-07-29 11:15:16 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,17 @@
-type Params = {
-  params: {
-    slug: string;
-  };
-};
+interface PageProps {
+  params: { slug: string }
+}
 
-export async function generateMetadata({ params }: Params) {
+export async function generateMetadata({ params }: PageProps) {
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>HoldText</p><p>Welcome to Hcm</p></>;
+export default function Page({ params }: PageProps) {
+  return (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>HoldText</p>
+      <p>Welcome to Hcm</p>
+    </>
+  );
 }
